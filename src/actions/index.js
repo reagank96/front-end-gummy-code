ec541a1d32916b214d928f13316362132dcbc8c5
ReaganKeith <reagankeith25@gmail.com>
2022-02-22 19:30:53 -0700

    project

---
+++ src/actions/index.js
@@ -0,0 +1,32 @@
+import { 
+    setHeaderLinks,
+    setNavbarLinks,
+    changeNavbarActive
+} from './headernavbar';
+
+import {
+    fetchUserPurchases,
+    setPurchaseDetail
+} from './user';
+
+import {
+    fetchShopCategories,
+    fetchShopProducts,
+    filterProductsWithCategoryId,
+    filterProductsWithQuery
+} from './shop';
+
+export {
+    setHeaderLinks,
+    setNavbarLinks,
+    changeNavbarActive,
+
+    fetchUserPurchases,
+    setPurchaseDetail,
+
+    fetchShopCategories,
+    fetchShopProducts,
+    filterProductsWithCategoryId,
+
+    filterProductsWithQuery
+};
